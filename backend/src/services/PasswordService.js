c3317a1ef14f363cd17a73dd2f36b29203de7d05
lucianaMoreiraAilveira <amigopet37@gmail.com>
2025-09-23 15:21:45 -0300

    atualizarndo passwordservice

--- backend/src/services/PasswordService.js
+++ backend/src/services/PasswordService.js
@@ -25,7 +25,8 @@ class PasswordService {
 
     await this.userRepository.updateUser(user);
 
-   const resetUrl = `https://amigopet.onrender.com/redefinir-senha.html?token=${token}`;
+   const resetUrl = `https://amigopet-d0856.web.app/redefinir-senha.html?token=${token}`;
+   
 
     await this.transporter.sendMail({
       from: process.env.EMAIL_USER,
